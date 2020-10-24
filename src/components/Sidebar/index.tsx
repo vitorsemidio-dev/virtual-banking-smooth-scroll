f296ad2cbd163c9c7f93e076d47f526000de3d5f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-23 23:50:54 -0300

    SidebarWrapper

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,7 +2,16 @@
 
 import React from 'react';
 
-import { CloseIcon, SidebarContainer, Icon } from './styles';
+import {
+  CloseIcon,
+  SidebarContainer,
+  Icon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRouter,
+} from './styles';
 
 const Sidebar: React.FC = () => {
   return (
@@ -10,6 +19,18 @@ const Sidebar: React.FC = () => {
       <Icon>
         <CloseIcon></CloseIcon>
       </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='about'>About</SidebarLink>
+          <SidebarLink to='discover'>Discover</SidebarLink>
+          <SidebarLink to='services'>Services</SidebarLink>
+          <SidebarLink to='signup'>Sign up</SidebarLink>
+        </SidebarMenu>
+
+        <SideBtnWrap>
+          <SidebarRouter to='/signin'>Sign In</SidebarRouter>
+        </SideBtnWrap>
+      </SidebarWrapper>
     </SidebarContainer>
   );
 };
