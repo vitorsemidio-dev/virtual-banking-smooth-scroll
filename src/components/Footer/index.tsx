feeea321a501af9cdc8518a5c9cc3a90a3692ad8
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-24 16:34:50 -0300

    Sessão SocialMedia do Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,13 @@
 /** @format */
 
 import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
 
 import {
   FooterContainer,
@@ -70,29 +77,39 @@ const Footer: React.FC = () => {
             </WebsiteRights>
 
             <SocialIcons>
-              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
-                {/* <FaFacebook /> */}
-                FaFacebook
+              <SocialIconLink
+                href='https://www.facebook.com/'
+                target='_blank'
+                aria-label='Facebook'>
+                <FaFacebook />
               </SocialIconLink>
 
-              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
-                {/* <FaInstagram /> */}
-                FaInstagram
+              <SocialIconLink
+                href='https://www.instagram.com/'
+                target='_blank'
+                aria-label='Instagram'>
+                <FaInstagram />
               </SocialIconLink>
 
-              <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
-                {/* <FaTwitter /> */}
-                FaTwitter
+              <SocialIconLink
+                href='https://twitter.com/'
+                target='_blank'
+                aria-label='Twitter'>
+                <FaTwitter />
               </SocialIconLink>
 
-              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
-                {/* <FaYoutube /> */}
-                FaYoutube
+              <SocialIconLink
+                href='https://www.youtube.com/'
+                target='_blank'
+                aria-label='Youtube'>
+                <FaYoutube />
               </SocialIconLink>
 
-              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
-                {/* <FaLinkedin /> */}
-                FaLinkedin
+              <SocialIconLink
+                href='https://www.linkedin.com/'
+                target='_blank'
+                aria-label='Linkedin'>
+                <FaLinkedin />
               </SocialIconLink>
             </SocialIcons>
           </SocialMediaWrap>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -68,14 +68,48 @@ export const FooterLink = styled(Link)`
   }
 `;
 
-export const SocialMedia = styled.div``;
+export const SocialMedia = styled.section`
+  max-width: 1000px;
+  width: 100%;
+`;
 
-export const SocialMediaWrap = styled.div``;
+export const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 40px auto 0 auto;
 
-export const SocialLogo = styled(Link)``;
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
 
-export const WebsiteRights = styled.div``;
+export const SocialLogo = styled(Link)`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  font-weight: bold;
+`;
 
-export const SocialIcons = styled.div``;
+export const WebsiteRights = styled.small`
+  color: #fff;
+  margin-bottom: 16px;
+`;
 
-export const SocialIconLink = styled.a``;
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+export const SocialIconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+`;
