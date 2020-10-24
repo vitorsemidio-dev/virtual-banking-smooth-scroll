9f3d829cf7be3b92531553d10208f1c96a030add
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-23 23:07:06 -0300

    Navbar responsiva

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,19 @@
 /** @format */
 
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
 
-import { Nav, NavContainer, NavLogo } from './styles';
+import {
+  Nav,
+  NavContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from './styles';
 
 const Navbar: React.FC = () => {
   return (
@@ -10,6 +21,28 @@ const Navbar: React.FC = () => {
       <Nav>
         <NavContainer>
           <NavLogo to='/'>logo</NavLogo>
+
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='discover'>Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='signup'>Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
         </NavContainer>
       </Nav>
     </>
